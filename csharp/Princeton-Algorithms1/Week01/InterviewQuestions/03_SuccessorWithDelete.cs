78eb511b7c97bfbcb2f889edecd09dafbed2300d
Ozsvárt Károly <ozsvart.karoly@gmail.com>
2020-06-09 09:26:14 +0200

    Implement SuccessorWithDelete

---
+++ csharp/Princeton-Algorithms1/Week01/InterviewQuestions/03_SuccessorWithDelete.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Princeton_Algorithms1.Week01.InterviewQuestions
+{
+    /// <summary>
+    ///     Successor with delete.
+    ///     Given a set of n integers S={0,1,...,n−1} and a sequence of requests of the following form:
+    ///     
+    ///     - Remove x from S
+    ///     - Find the successor of x: the smallest y in S such that y ≥ x.
+    ///     
+    ///     design a data type so that all operations (except construction)
+    ///     take logarithmic time or better in the worst case.
+    /// </summary>
+    public sealed class _03_SuccessorWithDelete
+    {
+        private readonly int[] id;
+
+        public _03_SuccessorWithDelete(int n)
+        {
+            if (n < 1)
+                throw new ArgumentOutOfRangeException(nameof(n), n, "Value cannot be less than 1.");
+
+            id = new int[n];
+            for (int i = 0; i < id.Length; i++)
+                id[i] = i;
+        }
+
+        public void Remove(int x)
+        {
+            Union(x, x + 1);
+        }
+
+        public int SuccessorOf(int x)
+        {
+            return RootOf(x + 1);
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return RootOf(p) == RootOf(q);
+        }
+
+        private int RootOf(int x)
+        {
+            while (x != id[x])
+            {
+                id[x] = id[id[x]];
+                x = id[x];
+            }
+            return x;
+        }
+
+        public void Union(int p, int q)
+        {
+            int pRoot = RootOf(p);
+            int qRoot = RootOf(q);
+            id[pRoot] = qRoot;
+        }
+    }
+}
---
+++ csharp/Test/Week01/03_SuccessorWithDeleteTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Princeton_Algorithms1.Week01.InterviewQuestions;
+using Xunit;
+
+namespace Test.Week01
+{
+    public class _03_SuccessorWithDeleteTests
+    {
+        [Fact]
+        public void Test()
+        {
+            const int capacity = 8;
+            _03_SuccessorWithDelete sut = new _03_SuccessorWithDelete(capacity);
+            for (int i = 0; i < capacity - 1; i++)
+                sut.SuccessorOf(i).Should().Be(i + 1);
+
+            sut.Remove(3);
+            sut.SuccessorOf(2).Should().Be(4);
+
+            sut.Remove(2);
+            sut.SuccessorOf(1).Should().Be(4);
+
+            sut.Remove(4);
+            sut.SuccessorOf(1).Should().Be(5);
+
+            sut.Remove(6);
+            sut.SuccessorOf(5).Should().Be(7);
+
+            sut.Remove(5);
+            sut.SuccessorOf(1).Should().Be(7);
+            sut.SuccessorOf(0).Should().Be(1);
+        }
+    }
+}
