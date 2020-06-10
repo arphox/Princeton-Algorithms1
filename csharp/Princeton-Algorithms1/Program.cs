7dd56a5a7e421f21ccd7e705d09583a0bc6d6c3e
Ozsvárt Károly <ozsvart.karoly@gmail.com>
2020-06-10 09:34:15 +0200

    Implement PercolationStats

--- csharp/Princeton-Algorithms1/Program.cs
+++ csharp/Princeton-Algorithms1/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Princeton_Algorithms1
+﻿using System;
+using System.Diagnostics;
+using Princeton_Algorithms1.Week01.Percolation;
+
+namespace Princeton_Algorithms1
 {
     internal class Program
     {
         private static void Main()
         {
+            Stopwatch sw = Stopwatch.StartNew();
+            PercolationStats ps = new PercolationStats(770, 100); // with size 770, 100 trials take ~10s (in DEBUG)
+            Console.WriteLine(ps.Mean());
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadLine();
         }
     }
 }
---
+++ csharp/Princeton-Algorithms1/Week01/Percolation/PercolationStats.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Princeton_Algorithms1.Common;
+
+namespace Princeton_Algorithms1.Week01.Percolation
+{
+    public sealed class PercolationStats
+    {
+        private double[] results;
+        private readonly int _totalNumberOfElements;
+
+        public Func<IUnionFindImplementation> UnionFindImplementationFactory { get; set; }
+            = () => new WeightedPathCompressingQuickUnionFind();
+
+        /// <summary>
+        ///     Performs independent trials on an n-by-n grid
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="trials"></param>
+        public PercolationStats(int n, int trials)
+        {
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException(nameof(n), n, "Value cannot be less than 1");
+            if (trials <= 0)
+                throw new ArgumentOutOfRangeException(nameof(trials), trials, "Value cannot be less than 1");
+
+            _totalNumberOfElements = n * n;
+            RunTrials(n, trials);
+        }
+
+        private void RunTrials(int n, int trials)
+        {
+            results = new double[trials];
+            Parallel.For(
+                fromInclusive: 0,
+                toExclusive: trials,
+                body: index =>
+                {
+                    results[index] = RunTrial(n);
+                }
+            );
+        }
+
+        private double RunTrial(int n)
+        {
+            Percolation percolator = new Percolation(n, UnionFindImplementationFactory());
+            while (!percolator.Percolates())
+            {
+                int row = ThreadLocalRandom.Next(n); // TODO: move random to dependency
+                int col = ThreadLocalRandom.Next(n); // TODO: move random to dependency
+                percolator.Open(row, col);
+            }
+            double openCount = percolator.NumberOfOpenSites();
+            return openCount / (_totalNumberOfElements);
+        }
+
+        // sample mean of percolation threshold
+        public double Mean()
+        {
+            return results.Average();
+        }
+
+        // sample standard deviation of percolation threshold
+        public double Stddev()
+        {
+            throw new NotImplementedException();
+        }
+
+        // low endpoint of 95% confidence interval
+        public double ConfidenceLo()
+        {
+            throw new NotImplementedException();
+        }
+
+        // high endpoint of 95% confidence interval
+        public double ConfidenceHi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
