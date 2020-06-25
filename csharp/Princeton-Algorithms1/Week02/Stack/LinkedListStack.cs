eb296b0f21a37236ef510c62837b8584014d1771
Ozsvárt Károly <ozsvart.karoly@gmail.com>
2020-06-25 07:54:55 +0200

    Implement LinkedListStack

--- csharp/Princeton-Algorithms1/Week02/Stack/LinkedListStack.cs
+++ csharp/Princeton-Algorithms1/Week02/Stack/LinkedListStack.cs
@@ -1,34 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Princeton_Algorithms1.Week02.Stack
 {
     public sealed class LinkedListStack<T> : IStack<T>
     {
-        private Stack<T> stack = new Stack<T>();
+        private Node? first;
 
-        public int Count => stack.Count;
+        public int Count { get; private set; } = 0;
 
-        public IEnumerator<T> GetEnumerator()
+        public IEnumerator<T> GetEnumerator() => IterateAllItems().GetEnumerator();
+
+        private IEnumerable<T> IterateAllItems()
         {
-            return stack.GetEnumerator();
+            Node? current = first;
+            while (current != null)
+            {
+                yield return current.value;
+                current = current.next;
+            }
         }
 
         public T Peek()
         {
-            return stack.Peek();
+            if (first == null)
+                throw EmptyStack();
+            return first.value;
         }
 
         public T Pop()
         {
-            return stack.Pop();
+            if (first == null)
+                throw EmptyStack();
+
+            T item = first.value;
+            first = first.next;
+            Count--;
+            return item;
         }
 
         public void Push(T item)
         {
-            stack.Push(item);
+            var newNode = new Node { value = item };
+            newNode.next = first;
+            first = newNode;
+            Count++;
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private InvalidOperationException EmptyStack()
+        {
+            return new InvalidOperationException("Stack is empty");
+        }
+
+        private sealed class Node
+        {
+            internal T value;
+            internal Node? next;
+        }
     }
 }
